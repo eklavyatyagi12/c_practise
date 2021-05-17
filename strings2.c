7bf2988c84abedd4863fe1986a70ba6f9c4c9caa
eklavya <64527842+eklavyatyagi12@users.noreply.github.com>
2021-05-17 10:35:16 +0530

    Add files via upload

---
+++ strings2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    char st[50];
+    printf("Enter the string");
+    scanf("%s",&st);
+    printf("\n");
+    printf("the string you entered is:");
+    printf("%s",st);
+
+
+
+}
